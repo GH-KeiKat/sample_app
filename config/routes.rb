1dba45a48a6a4b657395d1f8c6dc7df2cc9c9285
kei-K <Kei.K.book.555@gmail.com>
2019-01-16 14:07:56 +0000

    Update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-  
-  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
   root 'static_pages#home'
+  
+  #get 'static_pages/home'
+  #help_path -> '/home'
+  #help_url  -> 'http://www.example.com/home'
+  #get  '/home', to: 'static_pages#home'
+
+  #get 'static_pages/help'
+  get  '/help', to: 'static_pages#help'
+
+  #get 'static_pages/about'
+  get  '/about', to: 'static_pages#about'
+  
+  #get 'static_pages/contact'
+  get  '/contact', to: 'static_pages#contact'  
 
 end
